cf1a58823d9efd0cb53c5c25daed00f13cc3dbb2
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-07-24 07:52:23 +0700

    First

---
+++ README.md
@@ -0,0 +1,1 @@
+# API Lets Cooking App
---
+++ db.js
@@ -0,0 +1,33 @@
+const artikels = require("./data/artikels");
+const items = require("./data/items");
+
+
+module.exports = {
+  artikels,
+  items,
+}
+
+
+// const items = require("./data/items");
+
+// module.exports = () => {
+
+//   var tags = items.map((i) => i.tags);
+//   tags = [].concat.apply([], tags);
+//   tags = [...new Set(tags)];
+
+ 
+
+//   var brands = items.map((i) => i.manufacturer);
+//   brands = [...new Set(brands)];
+
+//   var itemTypes = items.map((i) => i.itemType);
+//   itemTypes = [...new Set(itemTypes)];
+
+//   return {
+//     items,
+//     tags,
+//     brands,
+//     itemTypes,
+//   };
+// };
